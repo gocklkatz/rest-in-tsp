ddbd4303b5eee2d9defb5c8675deacb3173d39a3
Stefan Katzensteiner <stefan.katzensteiner@mailbox.org>
2021-08-28 18:41:46 +0200

    Added Spring Boot Project

---
+++ src/main/java/io/gocklkatz/tsp/RestInTsp/RestInTspApplication.java
@@ -0,0 +1,13 @@
+package io.gocklkatz.tsp.RestInTsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestInTspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestInTspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/gocklkatz/tsp/RestInTsp/RestInTspApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gocklkatz.tsp.RestInTsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestInTspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
