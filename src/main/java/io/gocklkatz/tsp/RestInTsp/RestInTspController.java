412cc45e39ef6816fb3900e46b3f623d8709ecef
Stefan Katzensteiner <stefan.katzensteiner@mailbox.org>
2021-08-28 18:49:01 +0200

    Commented structure for endpoint /tsp

--- src/main/java/io/gocklkatz/tsp/RestInTsp/RestInTspController.java
+++ src/main/java/io/gocklkatz/tsp/RestInTsp/RestInTspController.java
@@ -6,6 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RestInTspController {
 
+    /*
+    /v1/tsp POST: saves new tsp instance and starts computation
+        returns id of tsp instance
+	/v1/tsp/{id}: get result (shortest route, cost)
+        { status: "finished", route: "ABCD", cost: 4 }
+        { status: "computing" }
+        { status: "error", message: "Cost AC missing!" }
+     */
+
     @GetMapping(path = "/tsp")
     public String getTsp(){
         return "{ status: 'error', message: 'Not implemented yet!' }";
